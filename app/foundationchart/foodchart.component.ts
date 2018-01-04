df4df55b64cfc7c95377e0d9b3c26b44e55c5921
wisdom838 <35066925+wisdom838@users.noreply.github.com>
2018-01-04 17:06:39 +0530

    Add files via upload

---
+++ app/foundationchart/foodchart.component.ts
@@ -0,0 +1,97 @@
+import {Component, OnInit,ViewChild} from '@angular/core';
+import {MenuCategoryService} from '../services/menucategory.service';
+import {SubMenuCategoryService} from '../services/submenucategory.service';
+import {FooditemmasterService}  from '../services/fooditemmaster.service';
+
+@Component({
+  selector: 'app-foodchart',
+  templateUrl: './foodchart.component.html',
+  styleUrls: ['./foodchart.component.css'],
+  providers:[MenuCategoryService,SubMenuCategoryService,FooditemmasterService]
+})
+export class FoodchartComponent implements OnInit {
+  @ViewChild('frm') frm;
+  Foodchart="";
+  MenuCat="";
+  SubMenuCat="";
+  Items:any;
+  dataarry:any=[];
+
+  returnJsonMenu:any=[];
+  returnJsonSubMenu:any=[];
+  returnJsonitem :any;
+  selectedItems: any = [];
+  Arr = Array; //Array type captured in a variable
+  num:number = 10;
+  foodbox:boolean=true;
+  
+  constructor(private _menuservice:MenuCategoryService, private _submenuservice:SubMenuCategoryService, private _itemservice:FooditemmasterService) { 
+    this.getAllFoodItemMaster();
+  }
+
+  ngOnInit() {
+        this.getAllMenuCategory(); this.getAllSubMenuCategory(); 
+  }
+ 
+  onSubmit(data){
+     this.saveValues(data);
+  }
+  saveValues(data){
+    
+    data.item_ids=this.selectedItems;
+    
+    //console.log(this.returnJsonitem);
+    console.log(data);
+    //console.log(this.frm);
+  }
+
+  additem(recitem, event) {
+            var index = this.selectedItems.indexOf(recitem.item_id);
+            if (event.target.checked) {
+                if (index === -1) {
+                    this.selectedItems.push(recitem.item_id); 
+                }
+            } else {
+                if (index !== -1) {
+                    this.selectedItems.splice(index, 1);
+                }
+            }
+            console.log(this.selectedItems);
+        }
+    
+
+  getAllMenuCategory(): void{
+    // console.log("Hello fetch data ");        
+     this._menuservice.getMenu({"menu_categoryid":"0"}).subscribe(data => this.returnJsonMenu =data.data,
+     error=>alert(error),
+     () =>console.log("data displayed")
+     );
+  }
+
+
+  getAllSubMenuCategory(): void{
+    this._submenuservice.getsubMenu({"menu_sub_categoryid":"0"}).subscribe(data => this.returnJsonSubMenu =data.data,
+    error=>alert(error),
+    () =>console.log("data displayed")
+    );
+    
+ }
+   
+
+ getAllFoodItemMaster(): void{
+  this._itemservice.getFoodItemMasrter({"item_id":"0"}).subscribe(data => this.returnJsonitem =data.data,   
+  error=>alert(error),
+  () =>console.log("data displayed")
+ );
+ 
+}
+
+foodchart(){
+  if(this.foodbox ==true){
+  this.foodbox =false;
+  }
+  else{
+    this.foodbox =true;
+  }
+}
+}
